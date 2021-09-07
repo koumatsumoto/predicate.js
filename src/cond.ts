1a267c62c82981b174693c7aba4471d218b528df
Kou Matsumoto <developer@kou-matsumoto.jp>
2021-09-07 11:30:35 +0900

    fix: correct type definition of cond function

--- src/cond.ts
+++ src/cond.ts
@@ -2,15 +2,15 @@ import { ParamOf, Predicate, Refinement } from './predicate';
 
 export type Cond<F1 extends Predicate, F2 extends Predicate> = F1 extends Refinement<infer A1, infer B1>
   ? F2 extends Refinement<infer A2, infer B2>
-    ? Refinement<A1, B1 & B2>
+    ? Refinement<A1 & A2, B1 & B2>
     : F2 extends Predicate<infer A2>
-    ? Refinement<A1, A2 & B1>
+    ? Refinement<A1 & A2, A1 & A2 & B1>
     : never
   : F1 extends Predicate<infer A1>
   ? F2 extends Refinement<infer A2, infer B2>
-    ? Refinement<A1, A1 & B2>
+    ? Refinement<A1 & A2, A1 & A2 & B2>
     : F2 extends Predicate<infer A2>
-    ? Refinement<A1, A1 & A2>
+    ? Predicate<A1 & A2>
     : never
   : never;
 
